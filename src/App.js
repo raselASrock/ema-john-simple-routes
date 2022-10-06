321f94ccd1f9d39a64ce9c9eaba6313169a13fb6
raselASrock <mdraselm413@gmail.com>
2022-10-06 20:33:49 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>
+    }
+  ])
   return (
     <div>
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
